c34f43a916de88cccfafc0778c272c41eb6cc5c0
wyf <956895214@qq.com>
2015-11-08 00:55:36 +0900

    add a view templates (without JS)

---
+++ static/view/index.php
@@ -0,0 +1,13 @@
+<?php include("templates/_head.html"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>会员管理系统</title>
+	<link rel="stylesheet/less" type="text/css" href="../public/style/public.css">
+</head>
+<body>
+<?php include("templates/navbar.html"); ?>
+
+</body>
+</html>
+<?php include("templates/_footer.html"); ?>
