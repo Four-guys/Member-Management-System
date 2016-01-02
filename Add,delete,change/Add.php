2fc070f8278c88869bc3b0a5b8584efa63ff8df3
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-01-02 21:32:33 +0800

    Add&show

---
+++ Add,delete,change/Add.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title></title>
+</head>
+<body>
+  <?php
+
+  $Username = $_POST['Username'];
+  $Sex = $_POST['Sex'];
+  $Phonenumber = $_POST['Phonenumber'];
+  $Userlevel = $_POST['Userlevel'];
+  echo $Username;
+
+  $dbc = mysqli_connect('localhost','Eric','19950704w09','mms_t')
+    or die('数据插入失败，请重新插入1');
+
+
+  $query = "INSERT into userInfo  Values ('{$Username}','{$Sex}','{$Phonenumber}','{Userlevel}'";
+    
+
+  $result = mysqli_query($dbc,$query) or die('数据插入失败，请重新插入2');
+
+  mysqli_close($dbc);
+
+  echo '数据成功插入';
+  
+?>
+</body>
+</html>
+
+
+
